3a50bbb1f68a6aad6698d8d1b62ffdb1045a0a4f
umairny <umairny1@gmail.com>
2022-01-28 03:43:15 -0500

    tenzies game

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+#if you are deploying to https://umairny.github.io/mypf
+git push -f git@github.com:umairny/tenzies.git master:gh-pages
+
+cd -
